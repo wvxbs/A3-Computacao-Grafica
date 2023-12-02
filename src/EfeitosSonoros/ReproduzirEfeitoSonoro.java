ec86e5371228c29ae84eefe9df28fea75a248ab8
wvxbs <gabriel.ferreira7854@gmail.com>
2023-12-01 21:18:07 -0300

    comentários desncessários removidos

--- src/EfeitosSonoros/ReproduzirEfeitoSonoro.java
+++ src/EfeitosSonoros/ReproduzirEfeitoSonoro.java
@@ -15,30 +15,11 @@ public class ReproduzirEfeitoSonoro {
 
     // /construtores
     public ReproduzirEfeitoSonoro() {
-//        sons[0] = "sons/faz-o-l-vinheta.wav";
-//        sons[1] = "sons/blizzard-elimination.wav";
-//        sons[2] = "sons/brazil.wav";
-//        sons[3] = "sons/coin_1.wav";
-//        sons[4] = "sons/keep-on.wav";
-//        sons[5] = "sons/health.wav";
-//        sons[6] = "sons/ihu-bolsonaro.wav";
-//        sons[7] = "sons/jet-set-radio-cancel.wav";
-//        sons[8] = "sons/jet-set-radio-success.wav";
-//        sons[9] = "sons/faz-o-l-vinheta.wav";
-//        sons[10] = "sons/the-doom-slayer.wav";
-//        sons[11] = "sons/web_whatsapp.wav";
-//        sons[12] = "sons/whatsapp.wav";
 
         GerarListaDeEfeitosSonoros listaDeEfeitosSonoros = new GerarListaDeEfeitosSonoros();
         listaDeEfeitosSonoros.ExibirListaDeEfeitosSonoros();
         sons = listaDeEfeitosSonoros.getEfeitosSonoros();
     }
-
-    // construtor pra passar as músicas
-//    public ReproduzirEfeitoSonoro(String[] playlist) {
-//        // loop pra passar até 10 efeitos sonoros
-//        for (int i=0; (i<playlist.length && i <10); i++) {sons[i] = playlist[i];}
-//    }
 
     //geters e setters
     public boolean isTocando() {return tocando;}
