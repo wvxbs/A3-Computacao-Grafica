85a08b1b60285bde87c0b757b67c3dcccc80a03d
Gabriel Ferreira <gabriel.ferreira7854@gmail.com>
2023-12-03 18:03:04 -0300

    oops kk

--- src/Cena/Cena.java
+++ src/Cena/Cena.java
@@ -171,7 +171,7 @@ public class Cena implements GLEventListener {
 
         // configurando som
         reproduzirEfeitoSonoro = new ReproduzirEfeitoSonoro();
-        ReproduzirEfeitoSonoroEmLoop("Royal Days");
+        ReproduzirEfeitoSonoroEmLoop("02. Royal Days");
 
         mouseHabilitado = true;
     }
