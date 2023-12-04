d5f4c5c6bc99cf8c24fd0820297a60964613797a
Bruno Touzo <107005022+BrunoTouzo@users.noreply.github.com>
2023-12-03 22:32:26 -0300

    comit tal

--- src/Cena/Cena.java
+++ src/Cena/Cena.java
@@ -16,7 +16,7 @@ public class Cena implements GLEventListener {
     private GLU glu;
     private GLUT glut;
 
-    int largura, altura;
+    int largura=1920, altura=1080;
 
     //atributos de jogo
     public int pontos=0;
@@ -77,7 +77,7 @@ public class Cena implements GLEventListener {
 
         float[] tamq1 = {30,5}; // tamanho da barra
         float[] tamb1 = {3f,3f}; // tamanho do raio da bolinha
-        float[] tamq3 = {202,202}; // tamanho do background
+        float[] tamq3 = {largura/5,altura/5}; // tamanho do background
         float[] tamq6 = {20,20}; // tamanho do obstáculo da fase 2
         float[] tamq7 = {10,10}; // tamanho do obstáculo da fase 2
         float[] tambtn1 = {25,15}; // tamanho do botão 1
