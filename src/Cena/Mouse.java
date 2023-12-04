09365f4506c87f7d9b6cf2c93e6e7859e35f43eb
Bruno Touzo <107005022+BrunoTouzo@users.noreply.github.com>
2023-12-03 23:55:05 -0300

    askasksakas

--- src/Cena/Mouse.java
+++ src/Cena/Mouse.java
@@ -15,10 +15,7 @@ public class Mouse implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        //if(cena.mouseHabilitado){
-        //    cena.mouseX = (float) ((e.getX() / 5) - cena.largura);
-        //    cena.mouseY = (float) ((e.getY() / 5) - cena.altura) * -1;
-        //}
+
     }
 
     @Override
@@ -33,26 +30,6 @@ public class Mouse implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-//        if (cena.mouseHabilitado) {
-//            System.out.println("mouse X: "+(float)((e.getX()/5)-100)+" | mouse Y: "+(float)((e.getY()/5)-100));
-//            // obtendo valores tratados de x e y do clique na tela
-//            cena.mouseX = (float)((e.getX()/5)-100);
-//            cena.mouseY = (float)((e.getY()/5)-100)*-1;
-//
-//            if (cena.mouseX > cena.jogador.getObjSprite().getIntervaloDireita()[0][0]) { // alterna a movimentação da bola palos
-//                cena.jogador.getObjSprite().setMovendo(true);
-//                cena.jogador.getObjSprite().setDirecao(Direcao.DIREITA);
-//            }
-//            if (cena.mouseX < cena.jogador.getObjSprite().getIntervaloEsquerda()[0][1]) { // alterna a movimentação da bola palos
-//                cena.jogador.getObjSprite().setMovendo(true);
-//                cena.jogador.getObjSprite().setDirecao(Direcao.ESQUERDA);
-//            }
-//            if ((float)((e.getX()/5)-100) >= cena.jogador.getObjSprite().getIntervaloDireita()[0][0] &&
-//                    e.getX() <= cena.jogador.getObjSprite().getIntervaloEsquerda()[0][1]) {
-//                cena.jogador.getObjSprite().setMovendo(false);
-//            }
-//        }
-//
     }
 
     @Override
